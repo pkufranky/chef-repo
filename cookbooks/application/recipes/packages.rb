a4378e75c1e5808bc2f1ab0b94fa399035d08302
Ping Yin <pkufranky@gmail.com>
2012-01-09 00:19:01 +0800

    application cookbook: add packages recipe

---
+++ cookbooks/application/recipes/packages.rb
@@ -0,0 +1,43 @@
+
+app = node.run_state[:current_app]
+
+## First, install any application specific packages
+if app['packages']
+  app['packages'].each do |pkg,ver|
+    package pkg do
+      action :install
+      version ver if ver && ver.length > 0
+    end
+  end
+end
+
+## Next, install any application specific gems
+if app['gems']
+  app['gems'].each do |gem,ver|
+    gem_package gem do
+      action :install
+      version ver if ver && ver.length > 0
+    end
+  end
+end
+
+if app['pears']
+  include_recipe "php"
+  app['pears'].each do |pear,ver|
+    php_pear pear do
+      action :install
+      version ver if ver && ver.length > 0
+    end
+  end
+end
+
+if app['pips']
+  include_recipe "python"
+  app['pips'].each do |pip,ver|
+    python_pip pip do
+      version ver if ver && ver.length > 0
+      virtualenv ve.path
+      action :install
+    end
+  end
+end
