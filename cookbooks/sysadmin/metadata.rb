146d069e81f48792d1c51b4c01ed9b6f49e7940b
Ping Yin <pkufranky@gmail.com>
2012-01-08 11:28:34 +0800

    sysadmin: add apt dependency and more meta

--- cookbooks/sysadmin/metadata.rb
+++ cookbooks/sysadmin/metadata.rb
@@ -1,6 +1,15 @@
-maintainer       "letou"
+maintainer       "Letou, Inc"
 maintainer_email "ping@eee168.com"
 license          "All rights reserved"
 description      "Installs/Configures sysadmin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe            "sysadmin", "Install sys admin tools"
+
+%w{ ubuntu debian }.each do |os|
+	  supports os
+end
+
+%w{ apt }.each do |cb|
+	  depends cb
+end
