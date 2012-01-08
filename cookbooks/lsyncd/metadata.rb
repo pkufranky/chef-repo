579c6cd8bf10d37c6704cee0abcec74afd9aba4f
Ping Yin <pkufranky@gmail.com>
2012-01-08 11:49:58 +0800

    Add lsyncd 0.0.1

---
+++ cookbooks/lsyncd/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Letou, Inc."
+maintainer_email "pkufranky@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures lsyncd"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
