d72f3638b70fa08c5194b7f1a07eb34022c04daf
Ping Yin <pkufranky@gmail.com>
2012-01-08 11:28:34 +0800

    Add sysadmin cookbook

---
+++ cookbooks/sysadmin/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "letou"
+maintainer_email "ping@eee168.com"
+license          "All rights reserved"
+description      "Installs/Configures sysadmin"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
