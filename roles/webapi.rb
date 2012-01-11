ba197fc53eb1f03b962875d8fb571b4092c4bc2b
Ping Yin <pkufranky@gmail.com>
2012-01-11 22:51:35 +0800

    webapi: add www-data user ssh keys

--- roles/webapi.rb
+++ roles/webapi.rb
@@ -3,5 +3,10 @@ description "webapi role"
 run_list(
   'recipe[mysql::client]',
   'recipe[application]',
-  'recipe[webapi]'
+  'recipe[webapi]',
+  'recipe[user::data_bag]'
+)
+override_attributes(
+  :users => ["www-data"],
+  :user => {:ssh_keygen => false}
 )
