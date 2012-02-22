21deaeb72ce117550f69fa90802dd0aa2cfa3113
Ping Yin <pkufranky@gmail.com>
2012-02-22 19:50:48 +0800

    Add role hongyun

---
+++ roles/hongyun.rb
@@ -0,0 +1,11 @@
+name "hongyun"
+description "hongyun role"
+run_list(
+  'recipe[application]',
+  'recipe[user::data_bag]',
+  'recipe[mysql::server]'
+)
+override_attributes(
+  :users => ["himalayas"],
+  :user => {:ssh_keygen => false}
+)
