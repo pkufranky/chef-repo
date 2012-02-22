3d32beccc9fed21c414f44c268348e153cad8f2b
Ping Yin <pkufranky@gmail.com>
2012-02-22 23:04:55 +0800

    hongyun role: add crawler and rabbitmq

--- roles/hongyun.rb
+++ roles/hongyun.rb
@@ -1,9 +1,12 @@
 name "hongyun"
 description "hongyun role"
 run_list(
+  'role[crawler]',
   'recipe[application]',
   'recipe[user::data_bag]',
-  'recipe[mysql::server]'
+  'recipe[mysql::server]',
+  'recipe[rabbitmq]',
+  'recipe[rabbitmq-management]'
 )
 override_attributes(
   :users => ["himalayas"],
