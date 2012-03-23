3d837c434661fac3acdeed076474ac614d03b250
Ping Yin <pkufranky@gmail.com>
2012-03-23 14:47:47 +0800

    cookbook crawler: add PIL

--- cookbooks/crawler/recipes/default.rb
+++ cookbooks/crawler/recipes/default.rb
@@ -4,7 +4,7 @@ package "python-sqlalchemy"
 package "python-mysqldb"
 
 include_recipe "python"
-%w{simplejson}.each do |pip|
+%w{simplejson PIL}.each do |pip|
 	python_pip pip do
 		action :install
 	end
